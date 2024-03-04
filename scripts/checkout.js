b96aebca79edd2907a3555471ea2f62b7f7b17ed
darshithv <darshithv3392@gmail.com>
2024-03-04 20:45:30 +0530

    add validation

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -151,20 +151,28 @@ document.querySelectorAll('.js-save-link')
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       
+      
+      const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
+      
+      const newQuantity = Number(quantityInput.value);
+
+      if (newQuantity < 0 || newQuantity >= 1000) {
+        alert('Quantity must be at least 0 and less than 1000');
+        return;
+      }
+      updateQuantity(productId, newQuantity);
+      
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
       container.classList.remove('is-editing-quantity');
 
-      const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
-
-      const newQuantity = Number(quantityInput.value);
-
       const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
       
       quantityLabel.innerHTML = newQuantity;
-
       
-      updateQuantity();
+      updateCartQuantity();
+
+
     });
 
   });
