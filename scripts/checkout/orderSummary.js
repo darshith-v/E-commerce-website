e05e6bf244d4bb5e6d9fa1e36aebc9a82c8e1f01
darshithv <darshithv3392@gmail.com>
2024-03-13 22:56:14 +0530

    created getDeliveryOption function,export & import

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -8,7 +8,7 @@ import {
 import { products, getProduct } from "../../data/products.js";
 import { formateCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 
 const today = dayjs();
 const deliveryDate = today.add(7, 'days');
@@ -25,13 +25,7 @@ export function renderOrderSummary() {
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    })
+    const deliveryOption = getDeliveryOption(deliveryOptionId)
 
     const today = dayjs();
 
